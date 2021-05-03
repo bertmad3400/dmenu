078d7d5261a1c30dfb024c818ed9abffcf687e27
bertmad3400 <skrivtilbertram@gmail.com>
2021-05-03 14:29:03 +0200

    Change color scheme to more muted, used by my dwm too

--- config.def.h
+++ config.def.h
@@ -7,11 +7,17 @@ static const char *fonts[] = {
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char bg_col[]			= "#282828";
+static const char fg_col[]			= "#ebdbb2";
+static const char green_col[]		= "#98971a";
+static const char red_col[]			= "#fb4934";
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ebdbb2", "#282828" },
-	[SchemeSel] = { "#ebdbb2", "#98971a" },
-	[SchemeOut] = { "#ebdbb2", "#8ec07c" },
+	[SchemeNorm] = { fg_col, bg_col },
+	[SchemeSel] = { bg_col, fg_col },
+	[SchemeOut] = { fg_col, red_col },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
